bad6e89c4ee902c7064440eb58503357a16d0d11
anon <anon@onionmail.com>
2025-09-23 20:58:30 +0200

    examples

--- allow-ping.sh
+++ allow-ping.sh
@@ -1,16 +1,20 @@
 uci add firewall rule
-uci set firewall.@rule[-1]._name=ping
-uci set firewall.@rule[-1].src=wan
-uci set firewall.@rule[-1].target=ACCEPT
-uci set firewall.@rule[-1].proto=ICMP
+uci set firewall.@rule[-1]._name='Allow-Ping'
+uci set firewall.@rule[-1].src='wan'
+uci set firewall.@rule[-1].target='ACCEPT'
+uci set firewall.@rule[-1].proto='ICMP'
+uci set firewall.@rule[-1].icmp_type='echo-request'
+uci set firewall.@rule[-1].family='ipv4'
 uci commit firewall
 service firewall restart
 
 uci add firewall rule
-uci set firewall.@rule[-1]._name=ping6
-uci set firewall.@rule[-1].src=wan6
-uci set firewall.@rule[-1].target=ACCEPT
-uci set firewall.@rule[-1].proto=ICMP
+uci set firewall.@rule[-1]._name='Allow Ping6'
+uci set firewall.@rule[-1].src='wan'
+uci set firewall.@rule[-1].target='ACCEPT'
+uci set firewall.@rule[-1].proto='ICMP'
+uci set firewall.@rule[-1].icmp_type='echo-request'
+uci set firewall.@rule[-1].family='ipv6'
 uci commit firewall
 service firewall restart
 
