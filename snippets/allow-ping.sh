5c007aa85a5863e1f3d83b230f0918e1350cf109
anonuser28 <anon@onionmail.com>
2025-09-25 16:15:19 +0200

    Update allow-ping.sh

--- snippets/allow-ping.sh
+++ snippets/allow-ping.sh
@@ -9,7 +9,7 @@ uci commit firewall
 service firewall restart
 
 uci add firewall rule
-uci set firewall.@rule[-1]._name='Allow Ping6'
+uci set firewall.@rule[-1]._name='Allow-Ping6'
 uci set firewall.@rule[-1].src='wan'
 uci set firewall.@rule[-1].target='ACCEPT'
 uci set firewall.@rule[-1].proto='ICMP'
