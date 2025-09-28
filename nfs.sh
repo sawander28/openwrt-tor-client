0a840eeaf97172c416a92cc7633160b187ee5b16
anon <anon@onionmail.com>
2025-09-28 11:38:15 +0200

    nfs

--- nfs.sh
+++ nfs.sh
@@ -5,4 +5,5 @@ mkdir -p /nfs/share
 mount -t nfs4 192.168.1.10:/srv/nfs /nfs/share
 
 cat << EOF > /etc/fstab
-192.168.1.10:/srv/nfs /nfs/share nfs4 nofail,nolock
+192.168.1.10:/srv/nfs /nfs/share nfs4
+EOF
