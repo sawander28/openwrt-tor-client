64e11d1a35a47cee79bb2b0530e6b6a89d59337f
anon <anon@onionmail.com>
2025-09-30 02:12:36 +0200

    flash

--- nfs.sh
+++ nfs.sh
@@ -3,7 +3,6 @@ opkg update
 opkg install nfs-utils kmod-fs-nfs-v4
 mkdir -p /nfs/share
 mount -t nfs4 192.168.1.10:/srv/nfs /nfs/share
+cat "192.168.1.10:/srv/nfs /nfs/share nfs4 auto" >> /etc/fstab
 
-cat << EOF > /etc/fstab
-192.168.1.10:/srv/nfs /nfs/share nfs4
-EOF
+
