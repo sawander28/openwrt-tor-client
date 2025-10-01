d58a3e1f4e30a01d423a3708f0b6f7a9f9c00337
anon <anon@onionmail.com>
2025-10-01 06:56:58 +0200

    ntp-server

--- tor-extras.sh
+++ tor-extras.sh
@@ -1,6 +1,5 @@
 # Enable ntp server
 uci set system.ntp.enable_server='1'
-uci set system.ntp.enable_server='1'
 uci set system.ntp.interface='lan'
 uci set system.@system[0].timezone='Europe/Berlin'
 uci commit system
